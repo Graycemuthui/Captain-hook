dcc2081056380ac57fe481b4358d38662d3cdefd
Grayce Muthui <graycemuthui20@gmail.com>
2023-04-06 20:43:43 +0300

    Style the products using tailwind

--- src/components/product.js
+++ src/components/product.js
@@ -16,16 +16,34 @@ const Product = () => {
   }, [products]);
 
   return (
-    <div className="products">
-      <h1>Our Collection</h1>
-      {products.map((product) => (
-        <div key={product.id} className="product-description">
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-          <img src={product.image_url} alt={product.name} />
-        </div>
-      ))}
+    <div className="bg-white">
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        OUR COLLECTION
+      </h2>
+      <div className=" px-4 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-3 xl:gap-x-10">
+        {products.map((product) => (
+          <div key={product.id} className="product-description">
+            <div className="mt-6">
+              <div className="group relative">
+                <div>
+                  <img src={product.image_url} alt={product.name} />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-lg text-red-700">{product.name}</h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {product.description}
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    ${product.price}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
