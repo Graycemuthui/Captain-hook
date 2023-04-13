c2de1f52155d966ae48d3313cbf17f1c0c941c86
Grayce Muthui <graycemuthui20@gmail.com>
2023-04-13 17:00:32 +0300

    Create the productdetail component

--- src/components/product.js
+++ src/components/product.js
@@ -2,28 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Product = () => {
-  const [products, setProducts] = useState([]);
+  const [product, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("http://127.0.0.1:4000/api/v1/products")
       .then((res) => res.json())
       .then((data) => setProducts(data));
-  }, [products]);
+  }, [product]);
 
   return (
     <div className="bg-white">
       <h2 className="text-2xl font-bold tracking-tight text-gray-900  text-center">
         OUR COLLECTION
       </h2>
-      <Link to="/product/:id">
-        <div className=" px-4 pt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-3 xl:gap-x-10 cursor-pointer ">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="product-description col-span-1 flex flex-col bg-white border-2 p-2 h-full"
-            >
-              <div className="mt-6">
-                <div className="group relative">
+      <div className=" px-4 pt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-3 xl:gap-x-10 cursor-pointer ">
+        {product.map((product) => (
+          <div
+            key={product.id}
+            className="product-description col-span-1 flex flex-col bg-white border-2 p-2 h-full"
+          >
+            <div className="mt-6">
+              <div className="group relative">
+                <Link to={`/product/${product.id}`}>
                   <div>
                     <img
                       src={product.image_url}
@@ -31,23 +31,23 @@ const Product = () => {
                       className="w-full h-64 object-cover"
                     />
                   </div>
-                  <div className="mt-4 flex justify-between">
-                    <div>
-                      <h3 className="text-lg text-red-700">{product.name}</h3>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {product.description}
-                      </p>
-                    </div>
-                    <p className="text-sm font-medium text-gray-900">
-                      ${product.price}
+                </Link>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-lg text-red-700">{product.name}</h3>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {product.description}
                     </p>
                   </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    ${product.price}
+                  </p>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-      </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/productDetail.js
+++ src/pages/productDetail.js
@@ -2,49 +2,45 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
-  // fetch data based on id
-  const [products, setProducts] = useState([]);
   const { id } = useParams();
+  const [product, setProducts] = useState([]);
 
   useEffect(() => {
     fetch(`http://127.0.0.1:4000/api/v1/products/${id}`)
       .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, [products]);
+      .then((data) => {
+        setProducts(data);
+      });
+  }, [id]);
 
   return (
-    <div class="bg-white">
-      <div class="pt-6">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            class="product-description col-span-1 flex flex-col bg-white border-2 p-2 h-full"
-          >
-            <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-              <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-                <img
-                  src={product.image_url}
-                  alt={product.name}
-                  class="h-full w-full object-cover object-center"
-                />
-              </div>
+    <div className="bg-white d-flex justify-content-center container mt-5">
+      <div className="pt-6">
+        <div className="product-description col-span-1 flex flex-col bg-white border-2 p-2 h-full">
+          <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
+            <div className="aspect-h-4 aspect-w-3 overflow-hidden rounded-lg lg:block">
+              <img
+                src={product.image_url}
+                alt={product.name}
+                className="h-80 w-full object-cover object-center"
+              />
+            </div>
 
-              <div class="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-                <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                  <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                    {product.name}
-                  </h1>
-                </div>
-                <div class="mt-4 lg:row-span-3 lg:mt-0">
-                  <h2 class="sr-only">Product information</h2>
-                  <p class="text-3xl tracking-tight text-gray-900">
-                    {product.price}
-                  </p>
-                </div>
+            <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+              <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                  {product.name}
+                </h1>
+              </div>
+              <div className="mt-4 lg:row-span-3 lg:mt-0">
+                <h2 className="sr-only">Product information</h2>
+                <p className="text-3xl tracking-tight text-gray-900">
+                  ${product.price}
+                </p>
               </div>
             </div>
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
